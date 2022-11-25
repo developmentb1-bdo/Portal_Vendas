5c628695c5809b6cfd49d8f30dfef3a9518aed3e
Joao Avelino <joao.avelino@bdo.com.br>
2022-11-25 10:15:51 -0300

    refactor(GrupoItem.DAL): adiciona conexão com o Hana

--- Arquitetura/SAPB1.SqlServerDAL/GrupoItem/GrupoItemDAL.cs
+++ Arquitetura/SAPB1.SqlServerDAL/GrupoItem/GrupoItemDAL.cs
@@ -5,57 +5,95 @@ using System.Text;
 using SAPB1.IDAL.GrupoItem;
 using SAPB1.DTO.GrupoItem;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace SAPB1.SqlServerDAL.GrupoItem
 {
     public class GrupoItemDAL : IGrupoItem
     {
-        SqlServerConexao _conexao;
-
-        public GrupoItemDAL()
-        {
-            _conexao = new SqlServerConexao();
-        }
 
         public IList<GrupoItemDTO> ObterTodos()
         {
-            StringBuilder stb = new StringBuilder();
-            stb.Append("SELECT * FROM OITB WHERE Locked = 'N' ORDER BY ItmsGrpNam");
-
-            SqlCommand cmd = new SqlCommand(stb.ToString(), _conexao.Conexao);
-
-            try
+            string tipoBD = ConfigurationManager.AppSettings["TipoBD"].ToString();
+            if (tipoBD == "Hana")
             {
-                _conexao.Conectar();
+                HanaConexao conexaoHana = new HanaConexao();
 
-                SqlDataReader rdr = cmd.ExecuteReader();
+                string query = $@"SELECT * FROM OITB WHERE ""Locked"" = 'N' ORDER BY ""ItmsGrpNam""";
 
-                IList<GrupoItemDTO> listGrupo = new List<GrupoItemDTO>();
-
-                if (rdr.HasRows)
+                try
                 {
-                    while (rdr.Read())
+                    conexaoHana.Connection();
+
+                    IList<GrupoItemDTO> listGrupo = new List<GrupoItemDTO>();
+
+                    DataTable dt = conexaoHana.ExecuteDataTable(query);
+
+                    if (dt.Rows.Count > 0)
                     {
-                        listGrupo.Add(new GrupoItemDTO()
+                        foreach (DataRow dr in dt.Rows)
                         {
-                            ItmsGrpCod = Convert.ToInt32(rdr["ItmsGrpCod"].ToString()),
-                            ItmsGrpNam = rdr["ItmsGrpNam"].ToString()
-                        });
+                            listGrupo.Add(new GrupoItemDTO()
+                            {
+                                ItmsGrpCod = Convert.ToInt32(dr["ItmsGrpCod"].ToString()),
+                                ItmsGrpNam = dr["ItmsGrpNam"].ToString()
+                            });
+                        }
                     }
+
+                    return listGrupo;
                 }
-
-                rdr.Close();
-                rdr.Dispose();
-
-                return listGrupo;
+                catch (Exception er)
+                {
+                    throw new Exception("Erro no banco de dados: " + er.Message);
+                }
+                finally
+                {
+                    conexaoHana.Dispose();
+                }
             }
-            catch (Exception er)
+            else
             {
-                throw new Exception("Erro no banco de dados: " + er.Message);
-            }
-            finally
-            {
-                _conexao.Desconectar();
+                SqlServerConexao _conexao = new SqlServerConexao();
+                StringBuilder stb = new StringBuilder();
+                stb.Append("SELECT * FROM OITB WHERE Locked = 'N' ORDER BY ItmsGrpNam");
+
+                SqlCommand cmd = new SqlCommand(stb.ToString(), _conexao.Conexao);
+
+                try
+                {
+                    _conexao.Conectar();
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    IList<GrupoItemDTO> listGrupo = new List<GrupoItemDTO>();
+
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            listGrupo.Add(new GrupoItemDTO()
+                            {
+                                ItmsGrpCod = Convert.ToInt32(rdr["ItmsGrpCod"].ToString()),
+                                ItmsGrpNam = rdr["ItmsGrpNam"].ToString()
+                            });
+                        }
+                    }
+
+                    rdr.Close();
+                    rdr.Dispose();
+
+                    return listGrupo;
+                }
+                catch (Exception er)
+                {
+                    throw new Exception("Erro no banco de dados: " + er.Message);
+                }
+                finally
+                {
+                    _conexao.Desconectar();
+                }
             }
         }
     }
