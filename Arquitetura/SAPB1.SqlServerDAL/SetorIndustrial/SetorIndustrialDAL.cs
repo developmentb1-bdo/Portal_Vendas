48272511682e61244a635c5e4ec46f27b2633f05
Joao Avelino <joao.avelino@bdo.com.br>
2022-12-02 13:51:49 -0300

    feat(SetorIndustrialDAL.cs): cria conexao com HANA

--- Arquitetura/SAPB1.SqlServerDAL/SetorIndustrial/SetorIndustrialDAL.cs
+++ Arquitetura/SAPB1.SqlServerDAL/SetorIndustrial/SetorIndustrialDAL.cs
@@ -5,34 +5,60 @@ using System.Text;
 using SAPB1.DTO.SetorIndustrial;
 using SAPB1.IDAL.SetorIndustrial;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace SAPB1.SqlServerDAL.SetorIndustrial
 {
-    public class SetorIndustrialDAL:ISetorIndustrial
+    public class SetorIndustrialDAL : ISetorIndustrial
     {
         public IList<SetorIndustrialDTO> Listar()
         {
-            StringBuilder stb = new StringBuilder();
-            stb.Append("SELECT * FROM OOND");
-
-            SqlServerConexao conexao = new SqlServerConexao();
-
-            SqlCommand cmd = new SqlCommand(stb.ToString(), conexao.Conexao);
-
-            try
+            string tipoBD = ConfigurationManager.AppSettings["TipoBD"].ToString();
+            string query = "SELECT * FROM OOND";
+            if (tipoBD == "Hana")
             {
-                conexao.Conectar();
+                HanaConexao conexaoHana = new HanaConexao();
 
-                return PopularDados(ref cmd);
+                try
+                {
+                    conexaoHana.Connection();
+                    return PopularDadosHana(query, conexaoHana);
+                }
+                catch (Exception err)
+                {
+                    throw new Exception(err.Message);
+                }
+                finally
+                {
+                    conexaoHana.Dispose();
+                }
             }
-            catch (SqlException er)
+            else
             {
-                throw new Exception("Erro no banco de dados: " + er.Message);
+                StringBuilder stb = new StringBuilder();
+                stb.Append("SELECT * FROM OOND");
+
+                SqlServerConexao conexao = new SqlServerConexao();
+
+                SqlCommand cmd = new SqlCommand(stb.ToString(), conexao.Conexao);
+
+                try
+                {
+                    conexao.Conectar();
+
+                    return PopularDados(ref cmd);
+                }
+                catch (SqlException er)
+                {
+                    throw new Exception("Erro no banco de dados: " + er.Message);
+                }
+                finally
+                {
+                    conexao.Desconectar();
+                }
             }
-            finally
-            {
-                conexao.Desconectar();
-            }
+
         }
 
         private IList<SetorIndustrialDTO> PopularDados(ref SqlCommand cmd)
@@ -54,6 +80,27 @@ namespace SAPB1.SqlServerDAL.SetorIndustrial
             }
 
             rdr.Close();
+
+            return listSetorIndustrial;
+        }
+
+        private IList<SetorIndustrialDTO> PopularDadosHana(string query, HanaConexao conexaoHana)
+        {
+            DataTable dt = conexaoHana.ExecuteDataTable(query);
+
+            IList<SetorIndustrialDTO> listSetorIndustrial = new List<SetorIndustrialDTO>();
+
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    SetorIndustrialDTO setorIndustrialDTO = new SetorIndustrialDTO();
+                    setorIndustrialDTO.IndCode = Convert.ToInt32(dr["IndCode"]);
+                    setorIndustrialDTO.IndName = dr["IndName"].ToString();
+
+                    listSetorIndustrial.Add(setorIndustrialDTO);
+                }
+            }
 
             return listSetorIndustrial;
         }
